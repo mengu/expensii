e09f55c50411c17b113c656a5cd2b9d2adfefa86
mengu <mengukagan@gmail.com>
2010-10-22 00:48:57 +0300

    edited the app name.

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,7 +6,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
-    'name' => 'My Web Application',
+    'name' => 'Expensii',
     // preloading 'log' component
     'preload' => array('log'),
     // autoloading model and component classes
