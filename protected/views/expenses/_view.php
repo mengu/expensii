bdc0e03921787a704e36c18fda73a4e1d8836a29
mengu <mengukagan@gmail.com>
2010-10-22 00:56:04 +0300

    gimme the expense data as table.

--- protected/views/expenses/_view.php
+++ protected/views/expenses/_view.php
@@ -1,28 +1,7 @@
-<div class="view">
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('user_id')); ?>:</b>
-	<?php echo CHtml::encode($data->user_id); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('category_id')); ?>:</b>
-	<?php echo CHtml::encode($data->category->name); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('expense_name')); ?>:</b>
-	<?php echo CHtml::encode($data->expense_name); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('cost')); ?>:</b>
-	<?php echo CHtml::encode($data->cost); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('dateline')); ?>:</b>
-	<?php echo CHtml::encode($data->dateline); ?>
-	<br />
-
-
-</div>
+<tr>
+    <td><?php echo CHtml::encode($data->expense_name); ?></td>
+    <td><?php echo CHtml::encode($data->category->name); ?></td>
+    <td><?php echo CHtml::encode($data->user->name); ?></td>
+    <td><?php echo CHtml::encode($data->cost); ?></td>
+    <td><?php echo CHtml::encode(date("d/m/Y h:i A", $data->dateline)); ?></td>
+</tr>
--- protected/views/expenses/index.php
+++ protected/views/expenses/index.php
@@ -11,7 +11,15 @@ $this->menu=array(
 
 <h1>Expenses</h1>
 
+<table cellpadding="6" cellspacing="1">
+<tr>
+    <th>Expense name</th>
+    <th>Category</th>
+    <th>User</th>
+    <th>Cost</th>
+    <th>When</th>
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
 )); ?>
+</table>
