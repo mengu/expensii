1f733fbe452b61eace6c89617d20d19988aeebea
mengu <mengukagan@gmail.com>
2010-10-23 16:22:25 +0300

    changed config to use srbac.

--- protected/config/main.php
+++ protected/config/main.php
@@ -13,12 +13,44 @@ return array(
     'import' => array(
         'application.models.*',
         'application.components.*',
+        'application.modules.srbac.controllers.SBaseController',
     ),
     'modules' => array(
         // uncomment the following to enable the Gii tool
         'gii' => array(
             'class' => 'system.gii.GiiModule',
             'password' => 'admin',
+        ),
+        'srbac' => array(
+            'userclass'=>'User', //default: User
+            'userid'=>'id', //default: userid
+            'username'=>'name', //default:username
+            'debug'=>true, //default :false
+            'pageSize'=>10, // default : 15
+            'superUser' =>'Authority', //default: Authorizer
+            'css'=>'srbac.css', //default: srbac.css
+            'layout'=>
+            'application.views.layouts.main', //default: application.views.layouts.main,
+            //must be an existing alias
+            'notAuthorizedView'=> 'srbac.views.authitem.unauthorized', // default:
+            //srbac.views.authitem.unauthorized, must be an existing alias
+            'alwaysAllowed'=>array(
+            //default: array()
+            'SiteLogin','SiteLogout','SiteIndex','SiteAdmin',
+            'SiteError', 'SiteContact'),
+            'userActions'=>array('Show','View','List'), //default: array()
+            'listBoxNumberOfLines' => 15, //default : 10
+            'imagesPath' => 'srbac.images', // default: srbac.images
+            'imagesPack'=>'noia', //default: noia
+            'iconText'=>true, // default : false
+            'header'=>'srbac.views.authitem.header', //default : srbac.views.authitem.header,
+            //must be an existing alias
+            'footer'=>'srbac.views.authitem.footer', //default: srbac.views.authitem.footer,
+            //must be an existing alias
+            'showHeader'=>true, // default: false
+            'showFooter'=>true, // default: false
+            'alwaysAllowedPath'=>'srbac.components', // default: srbac.components
+            // must be an existing alias
         ),
     ),
     // application components
@@ -28,7 +60,7 @@ return array(
             'allowAutoLogin' => true,
         ),
         // uncomment the following to enable URLs in path-format
-        'urlManager' => array(
+        /*'urlManager' => array(
             'urlFormat' => 'path',
             'showScriptName' => false,
             'rules' => array(
@@ -36,7 +68,7 @@ return array(
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ),
-        ),
+        ),*/
         // uncomment the following to use a MySQL database
         'db' => array(
             'connectionString' => 'mysql:host=localhost;dbname=yii_expenses',
@@ -46,7 +78,7 @@ return array(
             'charset' => 'utf8',
         ),
         'authManager'=>array(
-            'class'=>'CDbAuthManager',
+            'class'=>'application.modules.srbac.components.SDbAuthManager',
             'connectionID'=>'db',
         ),
         'errorHandler' => array(
