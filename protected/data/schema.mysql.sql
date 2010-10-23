c1ac5d0036c0538057584db93621c429b01c9bb8
mengu <mengukagan@gmail.com>
2010-10-23 16:36:51 +0300

    got me authorization data.

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,3 +1,5 @@
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `AuthAssignment`
 --
@@ -9,6 +11,13 @@ CREATE TABLE IF NOT EXISTS `AuthAssignment` (
   `data` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`itemname`,`userid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `AuthAssignment`
+--
+
+INSERT INTO `AuthAssignment` (`itemname`, `userid`, `bizrule`, `data`) VALUES
+('admin', '1', '', 's:0:"";');
 
 -- --------------------------------------------------------
 
@@ -25,6 +34,32 @@ CREATE TABLE IF NOT EXISTS `AuthItem` (
   PRIMARY KEY (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `AuthItem`
+--
+
+INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES
+('createExpense', 0, 'create expense', NULL, NULL),
+('updateExpense', 0, 'update expense', '', 's:0:"";'),
+('viewExpense', 0, 'view expense.', '', 's:0:"";'),
+('deleteExpense', 0, 'delete expense.', '', 's:0:"";'),
+('indexExpense', 0, 'expense index.', '', 's:0:"";'),
+('deleteCategory', 0, 'delete category.', '', 's:0:"";'),
+('createCategory', 0, 'create category', '', 's:0:"";'),
+('indexCategory', 0, 'index category.', '', 's:0:"";'),
+('updateCategory', 0, 'update category', '', 's:0:"";'),
+('admin', 2, 'administrator role.', '', 's:0:"";'),
+('user', 2, 'user role.', '', 's:0:"";'),
+('canUpdateCategory', 1, 'task for updating category.', '', 's:0:"";'),
+('canCreateCategory', 1, 'task for creating category.', '', 's:0:"";'),
+('canCreateExpense', 1, 'task for creating expense.', '', 's:0:"";'),
+('canViewExpense', 1, '', NULL, NULL),
+('canUpdateExpense', 1, '', NULL, NULL),
+('canDeleteExpense', 1, '', NULL, NULL),
+('canViewCategory', 1, 'task for viewing category.', '', 's:0:"";'),
+('canDeleteCategory', 1, 'task for deleting category.', '', 's:0:"";'),
+('viewCategory', 0, 'view category operation.', '', 's:0:"";');
+
 -- --------------------------------------------------------
 
 --
@@ -37,6 +72,29 @@ CREATE TABLE IF NOT EXISTS `AuthItemChild` (
   PRIMARY KEY (`parent`,`child`),
   KEY `child` (`child`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `AuthItemChild`
+--
+
+INSERT INTO `AuthItemChild` (`parent`, `child`) VALUES
+('admin', 'canCreateCategory'),
+('admin', 'canCreateExpense'),
+('admin', 'canDeleteCategory'),
+('admin', 'canDeleteExpense'),
+('admin', 'canUpdateCategory'),
+('admin', 'canUpdateExpense'),
+('admin', 'canViewCategory'),
+('admin', 'canViewExpense'),
+('canCreateCategory', 'createCategory'),
+('canCreateExpense', 'createExpense'),
+('canDeleteCategory', 'deleteCategory'),
+('canDeleteExpense', 'deleteExpense'),
+('canUpdateCategory', 'updateCategory'),
+('canUpdateExpense', 'updateExpense'),
+('canViewCategory', 'viewCategory'),
+('canViewExpense', 'viewExpense');
+
 
 -- --------------------------------------------------------
 
