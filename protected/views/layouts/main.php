93e18877ad9e27c5f24d69b7909a53e0489b44c1
mengu <mengukagan@gmail.com>
2010-10-26 02:23:01 +0300

    updated the navigation.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -45,8 +45,8 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Expenses', 'url'=>array('/expenses')),
+				array('label'=>'Categories', 'url'=>array('/categories')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
